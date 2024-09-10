1317d218a4ad5d08623ec8d106ab0cc995cc5e21
five <mikawu73@gmail.com>
2024-09-09 20:54:49 -0400

    1/2

---
+++ src/Multiple.java
@@ -0,0 +1,11 @@
+public class Multiple {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
